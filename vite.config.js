b141fa19ee5dbb233e4b5b51d10e5244bd921ab0
TarasDer <tderev.switch@gmail.com>
2023-09-16 00:40:04 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,9 @@ export default defineConfig({
   base: "/car-rental/",
   resolve: {
     alias: {
+      src: "/src",
       components: "/src/components",
+      assets: "/src/assets",
       pages: "/src/pages",
     },
   },
